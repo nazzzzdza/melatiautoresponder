2c7758da3a5d841ab57b71fd30864dd78f7c55d2
nazzzzdza <nnnarrytta@gmail.com>
2026-05-18 21:02:29 +0200

    Create linkp.js

---
+++ commands/linkp.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trigger: "!linkp",
+
+  async execute(message) {
+
+    await message.channel.send(
+      "https://cdn.discordapp.com/attachments/1505649185997721781/1506001017748193440/linked_1.gif?ex=6a0cac3e&is=6a0b5abe&hm=4067d537029be06fa998388f3a8f6ec9698d86bf1cbed1ccf9feac3b3f8ff932"
+    );
+  }
+};
