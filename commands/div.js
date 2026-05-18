84a8d3c12d44f28f245838195aeca0efcbbb1a00
nazzzzdza <nnnarrytta@gmail.com>
2026-05-18 21:01:54 +0200

    Create div.js

---
+++ commands/div.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trigger: "!div",
+
+  async execute(message) {
+
+    await message.channel.send(
+      "https://cdn.discordapp.com/attachments/1505649185997721781/1506001940058865664/tumblr_d742e201f2a7279fa97fc65c73b8a435_047ee384_2048.webp?ex=6a0cad1a&is=6a0b5b9a&hm=503204d35b9a9c141fe4ccc1d88d7e2a1820da56835887ebb6bcf2bf575c5345"
+    );
+  }
+};
